fdb4f304fdc8afc4e15a261d6060e165f45e8190
bishal <234aparnapaul@gmail.com>
2025-04-01 11:53:50 +0530

    j

--- backend/models/Game.js
+++ backend/models/Game.js
@@ -1,6 +1,37 @@
 const mongoose = require("mongoose");
 
-const gameModel = new mongoose.Schema({});
+const gameModel = new mongoose.Schema({
+  players:[
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  ],
+  questionSet:[{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "QuestionSet",
+    required: true,
+  }],
+  TotalViews:{
+  type:Number,
+  default:0,
+  min:0
+  },
+  startTime:{
+    type: Date,
+    default: Date.now,
+  },
+  endTime:{
+    type: Date,
+    default: null,
+  },
+  gameStatus:{
+    type: String,
+    enum: ["pending", "ongoing", "completed"],
+    default: "pending",
+  },
+});
 
 const Game = mongoose.model("Game", gameModel);
 
